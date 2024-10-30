0c3c7c787be3d8d5cedea178abf34f40ee5c9a07
Raheem Qudus <84246065+Quddos@users.noreply.github.com>
2024-10-31 00:08:36 +0530

    norm

--- components/WorkExperience copy 2.jsx
+++ components/WorkExperience copy 2.jsx
@@ -8,6 +8,7 @@ const workExperiences = [
   {
     id: 1,
     year: 2024,
+    newyear: "-current",
     company: "OUTLIER",
     role: "AI Training Engineer",
     description:
@@ -15,7 +16,8 @@ const workExperiences = [
   },
   {
     id: 2,
-    year: 2024,
+    year: 2023,
+    newyear: "-current",
     company: "EDYGRAD",
     role: "CTO - Software Engineer",
     description:
@@ -24,6 +26,7 @@ const workExperiences = [
   {
     id: 3,
     year: 2022,
+    newyear: " ",
     company: "BDNATURETECH",
     role: "Senior Software Developer",
     description:
@@ -32,6 +35,7 @@ const workExperiences = [
   {
     id: 4,
     year: 2012,
+    newyear: " ",
     company: "Elakk Technologies",
     role: "Junior Technical Developer",
     description:
@@ -40,6 +44,7 @@ const workExperiences = [
   {
     id: 6,
     year: 2021,
+    newyear: " ",
     company: "Brainlox (Triluxo) Technologies.",
     role: "Python Programming Language Instructor(Intern)",
     description:
@@ -48,6 +53,8 @@ const workExperiences = [
   {
     id: 7,
     year: 2018,
+    newyear: 2020,
+    newyear: "-2020",
     company: "Ark Technologies Group",
     role: "ATM Software Technical Engineer",
     description:
@@ -127,7 +134,7 @@ const WorkExperience = () => {
                   {exp.description}
                 </p>
                 <div className="text-sm text-gray-500">
-                <TypewriterText text={`Worked here in ${exp.year}`} />
+                <TypewriterText text={`Worked here in ${exp.year}  ${exp.newyear}`} />
                   {/* <TypewriterText  text={`Worked here in ${exp.year}`} /> */}
                 </div>
               </motion.div>
@@ -148,7 +155,7 @@ const WorkExperience = () => {
                   repeat: Infinity,
                 }}
               >
-                {exp.year}
+                {exp.year}- {exp.newyear}
               </motion.div>
             </div>
           </motion.div>
